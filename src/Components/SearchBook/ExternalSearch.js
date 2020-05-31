5611bd18647fa4a142b7e4c6f96cbb37ee2a96f1
HDwivedi <hemaasheesh@gmail.com>
2020-05-31 17:58:54 -0400

    Search page fixed

--- src/Components/SearchBook/ExternalSearch.js
+++ src/Components/SearchBook/ExternalSearch.js
@@ -6,40 +6,63 @@ class ExternalSearch extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            books: []
+            books: [],
+            inputText: null
         };
         this.onChange = this.onChange.bind(this);
         this.clickHandler = this.clickHandler.bind(this);
     }
 
     clickHandler(event) {
-        axios.get(`https://www.googleapis.com/books/v1/volumes?q=java`).then(response => {
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${this.state.inputText}`).then(response => {
             this.setState({books: response.data.items})
         })
     }
 
-    onChange(e){
-        this.setState({[e.target.name]:e.target.value})
+    onChange(e) {
+        this.setState({[e.target.name]: e.target.value})
     }
 
 
     renderBooks = () => {
-        return this.state.books.map(book => {
-            return (<div className="allbooksdisplayed">
-                <div className="card-deck">
-                    <div className="card">
-                        <img className="card-img-top" src={book.volumeInfo.imageLinks.thumbnail} alt=""/>
-                        <div className="card-body">
-                            <h4 className="card-title">{book.volumeInfo.title}</h4>
-                            <p className="card-text">By: {book.volumeInfo.author}</p>
-                            <p className="price-text">Price: {book.saleInfo.listPrice.amount}</p>
-                            <a className="btn btn-block addToCart" href={book.saleInfo.buyLink}>Buy</a>
+        return this.state.books
+            .map(data => {
+                const book = {
+                    title: "",
+                    author: "",
+                    thumbnail: "",
+                    amount: "",
+                    buyLink: ""
+                };
+                if (data.volumeInfo) {
+                    book.title = data.volumeInfo.title;
+                    book.author = data.volumeInfo.author;
+                    if (data.volumeInfo.imageLinks) {
+                        book.thumbnail = data.volumeInfo.imageLinks.thumbnail
+                    }
+                }
+                if (data.saleInfo) {
+                    book.amount = data.saleInfo.listPrice ? data.saleInfo.listPrice.amount : "";
+                    book.buyLink = data.saleInfo.buyLink
+                }
+                return book;
+            })
+            .map(book => {
+                return (<div className="allbooksdisplayed">
+                    <div className="card-deck">
+                        <div className="card">
+                            <img className="card-img-top" src={book.thumbnail} alt=""/>
+                            <div className="card-body">
+                                <h4 className="card-title">{book.title}</h4>
+                                <p className="card-text">By: {book.author}</p>
+                                <p className="price-text">Price: {book.amount}</p>
+                                <a className="btn btn-block addToCart" href={book.buyLink}>Buy</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>)
+                </div>)
 
-        });
+            });
     };
 
     render() {
@@ -51,7 +74,8 @@ class ExternalSearch extends Component {
                     <form id="myform">
                         <div className="input-field">
                             <input type="search" id="search-box" className="form-control" placeholder="Search for Books"
-                                   aria-describedby="button-addon2" value={this.state.inputText || ""}  onChange={this.onChange}/>
+                                   aria-describedby="button-addon2" onChange={this.onChange}
+                                   value={this.state.inputText}/>
 
                         </div>
                         <button className="btn btn-search" type="button" id="search-btn"
